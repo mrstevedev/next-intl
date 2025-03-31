83f58106c3e7caeb0cf68a0e3964508a390d53ef
Steven <stevendotpulido@gmail.com>
2025-03-31 15:53:19 -0700

    refactor: add locale to link

--- src/app/[locale]/components/Header.tsx
+++ src/app/[locale]/components/Header.tsx
@@ -1,13 +1,14 @@
-import { useTranslations } from "next-intl";
 import Link from "next/link";
+import { useLocale, useTranslations } from "next-intl";
 import LocaleSwitcher from "./LocaleSwitcher";
 
 export default function Header() {
   const t = useTranslations("Navigation");
+  const locale = useLocale();
   return (
     <header className="pt-2 pb-2">
       <nav className="flex justify-between items-center">
-        <Link href="/">{t("home")}</Link>
+        <Link href={`/${locale}`}>{t("home")}</Link>
         <LocaleSwitcher />
       </nav>
     </header>
